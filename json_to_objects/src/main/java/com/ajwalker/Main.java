4a733a3deef5d2925c1ef253877db5b3b8ef4c04
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-08-27 21:59:02 +0300

    API ve stream çalışması

--- json_to_objects/src/main/java/com/ajwalker/Main.java
+++ json_to_objects/src/main/java/com/ajwalker/Main.java
@@ -2,10 +2,8 @@ package com.ajwalker;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-import java.util.Scanner;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -44,7 +42,7 @@ import java.util.stream.Stream;
 */
 public class Main {
 	public static void main(String[] args) {
-		List<User> users = UserRepository.getUsers(10);
+		List<User> users = UserRepository.getUsers(100);
 		users.stream()
 		     .forEach(user -> {
 			     System.out.println();
@@ -70,16 +68,26 @@ public class Main {
 		System.out.println("SORU: 4");
 //		groupByCountry(users);
 		
+		System.out.println();
+		System.out.println("SORU: 5");
+//		groupByMails(users);
 		
-		//6.Kullanıcıları cinsiyetlerine göre ayırın ve her cinsiyetin yaş ortalamasını hesaplayın.
-		//7.30 yaşından küçük kullanıcıları bulun ve isimleriyle birlikte yaşlarını küçükten büyüğe sıralayarak
-		// listeyin.
-		//8.Kullanıcıların telefon numaralarının alan kodlarına göre gruplandırın ve her alan kodundan kaç kişi
-		// olduğunu gösterin.
-		//9.İsim ve soyisimlerinin toplam uzunluğu 15 karakterden fazla olan kullanıcıları bulun ve bu kullanıcıların
-		// tam adlarını büyük harfle yazdırın.
-		//10.Kullanıcıların doğum tarihlerine göre, hangi ayda kaç kişinin doğduğunu hesaplayın ve sonuçları ay
-		// isimlerine göre sıralayın.
+		System.out.println();
+		System.out.println("SORU: 6");
+//		averageAgeByGender(users);
+		
+		System.out.println();
+		System.out.println("SORU: 7");
+//		getYoungUsers(users);
+		
+		System.out.println();
+		System.out.println("SORU: 8");
+//		groupUserByAreaCode(users);
+		
+		System.out.println();
+		System.out.println("SORU: 9");
+		getFullNameLongerThan15(users);
+		
 	}
 	//1.Kullanıcılar arasında en yaşlı kişiyi bulun.
 	private static void showOldestPerson(List<User> users){
@@ -112,5 +120,63 @@ public class Main {
 		     });
 	}
 	//5.En çok kullanılan 5 e-posta servis sağlayıcısını (gmail.com, yahoo.com gibi) ve kullanım sayılarını bulun.
+	private static void groupByMails(List<User> users){
+		users.stream().collect(Collectors.groupingBy(User::getEmail)).forEach((s, users1)->{
+			System.out.println(s+" :");
+			int counter = 0;
+			for (User user: users1){
+				System.out.println( (++counter)+ "- "+user);
+			}
+			System.out.println();
+		});
+	}
+	//6.Kullanıcıları cinsiyetlerine göre ayırın ve her cinsiyetin yaş ortalamasını hesaplayın.
+	private static void averageAgeByGender(List<User> users){
+		AtomicInteger fAverage = new AtomicInteger(0);
+		AtomicInteger fCounter = new AtomicInteger(0);
+		AtomicInteger mCounter = new AtomicInteger(0);
+		AtomicInteger mAverage = new AtomicInteger(0);
+		users.stream().collect(Collectors.groupingBy(User::getGender)).forEach((gender,user)-> {
+			
+			for (User user1: user){
+				if (user1.getGender().equals("female")){
+					fAverage.addAndGet((int)user1.getDob().getAge());
+					fCounter.getAndIncrement();
+				}else if (user1.getGender().equals("male")){
+					mAverage.addAndGet((int)user1.getDob().getAge());
+					mCounter.getAndIncrement();
+				}
+			}
+			if (fAverage.get() != 0 && mAverage.get() != 0){
+				System.out.println("average female age: "+fAverage.get()/fCounter.get());
+				System.out.println("average male age: "+mAverage.get()/mCounter.get());
+			}
+		});
+	}
+	//7.30 yaşından küçük kullanıcıları bulun ve isimleriyle birlikte yaşlarını küçükten büyüğe sıralayarak
+	// listeyin.
+	private static void getYoungUsers(List<User> users){
+		users.stream()
+		     .filter(user -> user.getDob().getAge() < 30)
+		     .sorted(Comparator.comparingInt(user -> Math.toIntExact(user.getDob().getAge())))
+		     .map(user -> user.getDob().getAge() + " - " + user.getName().getFirst())
+		     .forEach(System.out::println);
+	}
+	//8.Kullanıcıların telefon numaralarının alan kodlarına göre gruplandırın ve her alan kodundan kaç kişi
+	private static void groupUserByAreaCode(List<User> users){
+		users.stream().collect(Collectors.groupingBy(User::getNat)).forEach((s, users1) -> {
+			int counter = 0;
+			for (User user : users1){
+				counter++;
+			}
+			System.out.println(s +" - "+ counter);
+		});
+	}
+	//9.İsim ve soyisimlerinin toplam uzunluğu 15 karakterden fazla olan kullanıcıları bulun ve bu kullanıcıların
+	// tam adlarını büyük harfle yazdırın.
+	private static void  getFullNameLongerThan15(List<User> users){
+		users.stream().filter(user -> user.getName().getFirst().length()+user.getName().getLast().length() > 16)
+		     .map(user -> user.getName().getFirst()+ " "+ user.getName().getLast()).forEach(System.out::println);
+	}
 	
 }
