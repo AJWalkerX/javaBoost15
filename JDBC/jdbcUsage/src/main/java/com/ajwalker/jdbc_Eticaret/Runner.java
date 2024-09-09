74a52b9c0b29e27187f2b32d20136905d5e7e39f
Alexander Joseph Walker <a.j.walker99@outlook.com>
2024-09-09 13:00:35 +0300

    Connection to postgresql codes added.

--- JDBC/jdbcUsage/src/main/java/com/ajwalker/jdbc_Eticaret/Runner.java
+++ JDBC/jdbcUsage/src/main/java/com/ajwalker/jdbc_Eticaret/Runner.java
@@ -12,12 +12,12 @@ public class Runner {
     public static void main(String[] args) {
         MusteriReposiyory musteriRepository = new MusteriReposiyory();
 //        Musteri m1 = new Musteri("Selim", "Sarı", "E", LocalDate.of(2000,5,15),"555555", "selim@gmail.com","İzmir");
-//        musteriReposiyory.save(m1);
-//        musteriReposiyory.delete(1001);
-//        musteriReposiyory.findAll().forEach(System.out::println);
-//        Optional<Musteri> byId = musteriReposiyory.findById(1);
+//        musteriRepository.save(m1);
+//        musteriRepository.delete(1001);
+//        musteriRepository.findAll().forEach(System.out::println);
+//        Optional<Musteri> byId = musteriRepository.findById(1);
 //        byId.ifPresent(System.out::println);
-        Optional<Musteri> guncellenecekMusteri = musteriRepository.findById(2008);
+        Optional<Musteri> guncellenecekMusteri = musteriRepository.findById(1002);
 		if(guncellenecekMusteri.isPresent()){
 			guncellenecekMusteri.get().setSoyad("YeniSarı");
 			guncellenecekMusteri.get().setEmail("selimyenisari@gmail.com");
@@ -27,12 +27,12 @@ public class Runner {
 		}
 
 
-        AdresRepository adresRepository = new AdresRepository();
-//        Adres adres = new Adres(1002, "İzmir", "Bostanlı", "Bostan","112 sk");
-//        adresRepository.save(adres);
-
-        Optional<Adres> byId = adresRepository.findById(1012);
-        byId.ifPresent(System.out::println);
+//        AdresRepository adresRepository = new AdresRepository();
+////        Adres adres = new Adres(1002, "İzmir", "Bostanlı", "Bostan","112 sk");
+////        adresRepository.save(adres);
+//
+//        Optional<Adres> byId = adresRepository.findById(1012);
+//        byId.ifPresent(System.out::println);
 
 //        adresRepository.findAll().forEach(System.out::println);
 
